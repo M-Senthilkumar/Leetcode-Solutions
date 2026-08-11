bee2954cbc1da2ca8da9603b0e129c3b0df77597
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:07:18 +0530

    Create 486-PredictTheWinner

---
+++ 486-PredictTheWinner/486-PredictTheWinner.java
@@ -0,0 +1,17 @@
+// Last updated: 8/11/2026, 10:07:18 PM
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[] max_diff = new int[n];
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            max_diff[i] = nums[i];
+            for (int j = i + 1; j < n; j++) {
+                max_diff[j] = Math.max(nums[i] - max_diff[j], nums[j] - max_diff[j - 1]);
+            }
+        }
+
+        return max_diff[n - 1] >= 0;        
+    }
+}
