e9edfdc36b90a0660b46b48913e4c10a4c85d4db
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:06:44 +0530

    Create 575-DistributeCandies

---
+++ 575-DistributeCandies/575-DistributeCandies.java
@@ -0,0 +1,17 @@
+// Last updated: 8/11/2026, 10:06:44 PM
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> set=new HashSet<>();
+        for(int n:candyType)
+        set.add(n);
+        int n=candyType.length/2;
+
+        if(set.size()>=n)
+        return n;
+        else
+        {
+            return set.size();
+        }
+
+    }
+}
