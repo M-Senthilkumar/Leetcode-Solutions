9f260ddfdb709a1ba8b12a1b2249289fbdf4ad85
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:10:04 +0530

    Create 171-ExcelSheetColumnNumber

---
+++ 171-ExcelSheetColumnNumber/171-ExcelSheetColumnNumber.java
@@ -0,0 +1,21 @@
+// Last updated: 8/11/2026, 10:10:04 PM
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+		int i = 0;
+		while (i < columnTitle.length() - 1) {
+			int value = columnTitle.charAt(i) - 'A' + 1;
+			int n = i;
+			while (n < columnTitle.length()-1) {
+				value *= 26;
+				n++;
+			}
+			result += value;
+			i++;
+		}
+		int value = columnTitle.charAt(i) - 'A' + 1;
+		result += value;
+		return result;
+
+    }
+}
