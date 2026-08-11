a4f04bf7dda87f8b80acefc25e9a133590987b90
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:11:11 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.java
@@ -0,0 +1,24 @@
+// Last updated: 8/11/2026, 10:11:11 PM
+class Solution {
+    public void sortColors(int[] nums) {
+        int mid=0,low=0,high=nums.length-1;
+        while(mid<=high)
+        {
+             if(nums[mid]==0)
+             {
+                int temp=nums[low];
+                nums[low++]=nums[mid];
+                nums[mid++]=temp;
+             }
+             else if(nums[mid]==1){
+             mid++;
+             }
+             else
+             {
+                int temp=nums[mid];
+                nums[mid]=nums[high];
+                nums[high--]=temp;
+             }
+        }
+    }
+}
