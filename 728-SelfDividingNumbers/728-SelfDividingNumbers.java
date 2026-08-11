30f01abf396a88b6a69abcb906e9955485c243ee
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:06:15 +0530

    Create 728-SelfDividingNumbers

---
+++ 728-SelfDividingNumbers/728-SelfDividingNumbers.java
@@ -0,0 +1,24 @@
+// Last updated: 8/11/2026, 10:06:15 PM
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans=new ArrayList<>();
+        for(int i=left;i<=right;i++)
+        {
+            int temp=i,d;
+            while(temp>0)
+            {
+               d=temp%10;
+               if(d==0 || i%d!=0)
+               {
+                    break;
+               }
+               temp/=10;
+            }
+            if(temp==0)
+            {
+                ans.add(i);
+            }
+        }
+       return ans;
+    }
+}
