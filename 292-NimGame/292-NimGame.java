fe9dfffd1f363ed876fc8491958e429d4d5b4ef6
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:08:20 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 8/11/2026, 10:08:20 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0){
+            return false;
+        }
+        return true;
+    }
+}
