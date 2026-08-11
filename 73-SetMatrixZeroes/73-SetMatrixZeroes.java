9ab97c0c5f321bbef17a1dec32bc902b9144ad62
Senthilkumar M <senthilmks007@gmail.com>
2026-08-11 22:11:19 +0530

    Create 73-SetMatrixZeroes

---
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.java
@@ -0,0 +1,30 @@
+// Last updated: 8/11/2026, 10:11:19 PM
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        Set<Integer> rows=new HashSet<>();
+        Set<Integer> cols=new HashSet<>();
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+             if(matrix[i][j]==0)
+             {
+                rows.add(i);
+                cols.add(j);
+             }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+             if(rows.contains(i) || cols.contains(j))
+             {
+                matrix[i][j]=0;
+             }
+            }
+        }
+    }
+}
