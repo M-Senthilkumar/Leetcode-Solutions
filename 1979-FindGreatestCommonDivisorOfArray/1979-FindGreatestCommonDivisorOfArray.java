350db32faa45202b200edbefd67c0288d15c566d
Senthilkumar M <senthilmks007@gmail.com>
2026-09-09 16:19:25 +0530

    Create 1979-FindGreatestCommonDivisorOfArray

---
+++ 1979-FindGreatestCommonDivisorOfArray/1979-FindGreatestCommonDivisorOfArray.java
@@ -0,0 +1,19 @@
+// Last updated: 9/9/2026, 4:19:24 PM
+class Solution {
+    public int findGCD(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num < min) min = num;
+            if (num > max) max = num;
+        }
+
+        return gcd(min, max);
+    }
+
+    public int gcd(int a, int b) {
+        if (a == 0) return b;         
+        return gcd(b % a, a);         
+    }
+}
